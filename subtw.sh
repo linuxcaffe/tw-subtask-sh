2265f41e42bd39d20b46280b94b644fe35701220
David J Patrick <davamundo@gmail.com>
2016-09-25 01:38:38 -0400

    Update subtw.sh

--- subtw.sh
+++ subtw.sh
@@ -4,3 +4,7 @@
 # function: subtw_create
 
 # function: subtw_invoke
+## function: next_subtask
+# task $ORIGIN_UUID add $*
+# task $ORIGIN_UUID mod .. update checkbox somehow.. s/[ ]/[o]/
+# task $ORIGIN_UUID mod dep:$SUB_TASK_UUID
